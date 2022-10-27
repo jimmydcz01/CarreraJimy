eb88e6b827108e624d0c6d2770f566ec8ce4211c
jimmydcz01 <jimy.carrera@utelvt.edu.ec>
2022-10-26 22:21:47 -0500

    ACTUALIZACIÓN DE INFORMACIÓN

--- index.php
+++ index.php
@@ -12,21 +12,21 @@ include("marco-right.php");
    <div  style="width: 100%;">     
     <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
        <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left;">
-        	<h1>¿Quién es Stalin Francis?</h1>
+        	<h1>¿Quién es Jimmy Carrera?</h1>
        		 <hr>
              
           <div id="posteo" style="text-align: left; font-size:0.5vw;">
-                Posteado por:  Stalin Francis<br>
-                Fecha: 2021-08-17 <br>
+                Posteado por:  Jimmy Carrera<br>
+                Fecha: 2022-10-26 <br>
           </div>
         </div>
     </div>
   </div>
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
-                   <p class="eys-parrafo">Hola, yo soy Stalin Francis, ingeniero esmeraldeño que labora desde hace 10 años en la Universidad Tecnica Luis Vargas Torres, e impartido diferentes cátedras en la carrera de Tecnología de la Información entre las cuales esta Ingeniería de Software, Programación Web, Proyectos informáticos entre otras; pero la cátedra en la que mejor me desempeño es la de Fundamentos de programación que se dicta en el primer ciclo de la carrera de Tecnología de la Información. </p>
-                   <p class="eys-parrafo">Desde mís estudios secundarios me he apacionado por las ciencias y la tecnología, quieza sería  por esta capacidad que tiene estas dos áres del conocimiento de solucionar muchos probemas de la sociedad; es por eso que desde que termine mis estudios de ingeniería en la ESPOL, me he dedicado a la programación de computadoras. </p>
-<p class="eys-parrafo"> El primer lenguaje con el cual empece a programar en el tercer ciclo de universidad fue el <b><a href="https://en.wikipedia.org/wiki/BASIC"> basic</a></b>  para este tiempo la ESPOL contaba con laboratorios de computadora  una computadora personal de marca IBM de tip XT modelo 5160  en es.</p>
+                   <p class="eys-parrafo">Hola, yo soy Jimmy Carrera, estudiante universitario esmeraldeño que estudia desde hace 3 años en la Universidad Tecnica Luis Vargas Torres, en la carrera de Tecnología de la Información.</p>
+                   <p class="eys-parrafo">Desde mís estudios primarios me he apacionado por hacer las cosas de la mejor manera: con responsabilidad, empeño, esfuerzo y sacrificio, teniendo siempre presente el aprender y capacitarme cada vez que veo nuevas cosas y poder tener esas ganas y cero conformismo de adquirir mas y hacerlo de manera propia. </p>
+<p class="eys-parrafo"> El primer lenguaje con el cual empece a programar en el primer ciclo de la universidad fue <b><a href="https://es.wikipedia.org/wiki/C%2B%2B"> C++</a></b>  para este tiempo la UTELVT contaba con laboratorios de computadora no en muy buen estado.</p>
 
    <div id="blog-seccion2"  style="height: 50px;"></div>
 
@@ -34,23 +34,21 @@ include("marco-right.php");
     
 
 
-                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Fisco-Misional Sagado Corazon donde me gradue en el año 1989, de la especialidad de Físico-Matemático, apenas me gradue viaje a Guayaquil para seguir mis estudios universitarios en la Escuela Superior Politécnica del Litoral donde seguí la carrera de Ingeniería en Computación, gracias a la demanda que exitía en este tiempo de este tipo de profesionales comencé a trabajar mientra estaba estudiando, prestando mis servicios profesionales en empresas como Poligráfica, El telegráfo, Intecorsorcio; mientras trabajaba en esta última logré obtener mi título de Ingeniero de Computación.</p>
-                   <p class="eys-parrafo">Ya con el título en la mano decidi regrasar a mi tierra Esmeraldas, con la finalidad de aplicar los conocimiento y habilidades obtenida por lo que forme la empresa EDUCAYSOFT, pero como los ingreso no fueron suficiente entre a trabajar al colegio Eloy Alfaro y posteiormente a la Universidad Técnica Luis Vargas Torres donde actualmente laboró.</p>
-<p class="eys-parrafo"> Por las exigencias de la universidad, ingresar a la maestria de docencia y desarrollo del curriculo, de la cual de grauduo obteniendo el título de Magister, pero despues realice otra maestría en la POLITECNICA, de Ciencias de la Computación.</p>
+                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Unidad Educativa Fiscal "Atacames" y en el colegio CAMILO PONCE ENRIQUEZ En la ciudad de Quito Ecuador donde me gradue en el año 2017, de la especialidad de CIENCIAS, apenas me gradue empece a trabajar un año en una empresa de TELECOMUNICACIONES para reunir dinero para poder estudiar en la Universidad Central del Ecuador pero resultó que Dios tuvo otros planes para mi vida y tuve la oportunidad de viajar a Esmeraldas por cosas personales lo cual me obligó poder estudiar en la Universidad Luis Vargas Torres de Esmeraldas.</p>
+                   
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
     
-
-
-                   <p  class="eys-parrafo">Mi perfil tecnológico es la programación de computadoras, mi primer programa lo hice en el leguaje Basic, utilizando el computador de la espol, luego programe c++, donde realiza algunos programa que leina el entregas del puerto serial y enviaban señales a un motor. Profesionalmente comenza programando en Informix un lenguaje de programación para el sistema Unix, utilizando uan versio de foxpro para unix, luego cuando aparecio la interface gráfica programe en foxpro y seguidamente visual foro.</p> 
-                   <p  class="eys-parrafo">Hoy en día creso sisemas web combinando HTML, CSS, JavaScrip y PHP; tambien aplicaciones para dispositivo androi para lo cual utilizo el framework android Studio, el cual utiliza el lenguaje de JAVA.</p>
+	
+             <p class="eys-parrafo">Hoy en día he utilizado lenguajes de programación de programación: C++, Dev C++, C#, JAVA, JavaScrip y PHP, y la etiqueta de marcas de Hipertexto HTML, donde he podido aplicar mis conocimientos aprendido con la guía de los profesores de Programación, realizando y desarrollando algunos miniproyectos para la finalización de cada semestre de la materia de Programación.</p>
+			 
    <div id="blog-seccion4"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
     
 
-                   <p   class="eys-parrafo">Soy amante a la lectura, así que en los ratos libres me encanta actualizar mis conocimientos leyendo manuales de los leguajes de progración que namejo, y tambien muchos noticias de las ultimas tendencias en tecnología.</p>
+                   <p   class="eys-parrafo">Soy amante a la poesía, apasionado del romanticismo, me encanta cantar y entonar la guitarra, me gusta la música romántica y expresar lo que siento en ese momento por lo que veo o el paisaje hermoso que se manifiesta. Me gusta también escribir mensajes de ánimo y motivar no solo a la juventud si no a las personas que necesiten un consejo o mensaje y que me siento apto para darselo sin ningún miedo, mirar televisión de vez en cuando, leer y cocinar, me encanta cocinar para mi familia y amigos, mejores amigos. También me gusta hacer demasiado deporte ya sea fútbol o natación, de vez en cuando aunque no tengo mucho tiempo Videojuegos.</p>
        
 
                    
