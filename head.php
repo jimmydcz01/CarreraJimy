969f6115f9c8b718047dc47e4652e753cb40eec1
jimmydcz01 <jimy.carrera@utelvt.edu.ec>
2022-11-19 22:23:10 -0500

    ACTUALIZACIÓN DE INFORMACIÓN

--- head.php
+++ head.php
@@ -10,15 +10,17 @@
 <head>
  <title> JimmyCarrera </title>
 <meta charset="utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-	
-	<link href="images/jimycon.ico" rel="shortcut icon" type="image/x-icon" />
-	<meta name="description" content="Biografía de Jimmy Carrera" />
+		<link href="images/jimycon.ico" rel="shortcut icon" type="image/x-icon" />
+		<meta name="description" content="Biografía de Jimmy Carrera" />
 		
+<meta property="og:image" content="https://highkickesmeraldas.com/CarreraJimy/images/fotoperfil.png" />
+	<meta property="og:image:width" content="100" />
+	<meta property="og:image:height" content="100" />
+<!----        <meta name="viewport" content="width=device-width, initial-scale=1" /> --->
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" type="text/css" href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/assets/css/eys.css" />   
-<link rel="stylesheet" type="text/css" href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/assets/src/prettify.css" />   
-<style>
+<link rel="stylesheet" type="text/css" href="https://highkickesmeraldas.com/assets/css/eys.css" />   
+<link rel="stylesheet" type="text/css" href="https://highkickesmeraldas.com/assets/src/prettify.css" />   
+<style
 #wrapper {
 	background: #fff2;
 }
